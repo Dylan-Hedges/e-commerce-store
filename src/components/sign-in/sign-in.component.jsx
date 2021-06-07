e71df68096ac460ae3c007d12892db5a0d0bd236
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-06-07 07:12:20 -0400

    Added sign-in-and-sign-up-page (user can log in and register), added sign-in component (user enters email and password into fields, uses forms and a local state)

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  //Executed when form is submitted
+  handleSubmit = event => {
+    //Prevents the forms default behaviour
+    event.preventDefault();
+    //Clears the email and password fields
+    this.setState({email: '', password: ''});
+  }
+  //Executed when the user types
+  handleChange = event => {
+    const {value, name} = event.target;
+    //Dynamically updates state - [name] will be either "email" or "password", changes depending on the field the user is typing in, allows us to reuse the same function for both
+    this.setState ({[name]: value})
+  }
+  render(){
+    return(
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
+          <label>Email</label>
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} required/>
+          <label>Password</label>
+          <input type="submit" value="Submit Form"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
