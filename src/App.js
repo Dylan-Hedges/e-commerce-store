7853342ea6d4742291d05aad6f87a36a1960958e
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-07-08 20:01:34 -0400

    Added functionality to update current user in the Reux Store - dispatches an action from App.js with the current user info object, updates current user info in the Redux Store, constructor removed for this component as we are no longer using local state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   //null property used for closing the firebase auth subscription
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     //Listens to changes in authentication on firebase - uses an Open Subscription, whenever there is a change in authentication (e.g. a user logs in/out) firebase sends a message saying the authentication state has changed
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       //If the user is signed in - i.e. if there is a userAuth object
       if (userAuth){
         //Executes the create a new user function and saves the user document reference- passes the user authentication object we get from firebase into this function
         const userRef = await createUserProfileDocument(userAuth);
-        //Saves the data stored in the document for this user to the state of this component - onSnapshot() subscribes/listens to the document for this user, on the inital call we get the current contents of the document, then each time the content changes another call updates the document snapshot
+        //Saves the data stored in the document for this user and passes it into the setCurrentUser action as the payload - onSnapshot() subscribes/listens to the document for this user, on the inital call we get the current contents of the document, then each time the content changes another call updates the document snapshot
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser:{
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       }else{
-        //Set state to null - there is no user auth object
-        this.setState({currentUser: userAuth})
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -60,4 +53,10 @@ class App extends React.Component {
   }
 }
 
-export default App;
+//Allows our component to dispatch actions to update the Redux Store
+const mapDispatchToProps = dispatch => ({
+  //Dispatches the action setCurrentUser with the type as 'SET_CURRENT_USER' and the payload as the user object, the 'SET_CURRENT_USER' reducer will then update the Redux Store with the info of the currently logged in user
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
