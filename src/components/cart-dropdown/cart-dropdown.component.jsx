82de04f3df4d80b7577cdaed355659ccc54946e5
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-07-20 08:09:07 -0400

    Added display items in Cart Dropdown Menu functionality - when user clicks add item it displays the item in the cart dropdown menu, added cart-item component to display items, mapped cartItems in global state to props of cart dropdown component, mapped over cartItems, used cart-item component to display items in the dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-//This is the dropdown menu when clicking shopping cart icon
-const CartDropdown = () => (
+//This is the dropdown menu when clicking shopping cart icon - .map( ) over the cartItems prop and uses <CartItem> to display them in the cart
+const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem}/>
+        ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
-)
+);
 
-export default CartDropdown;
+//Maps the global state to this component - saves cart items to a cartItems prop on this component
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
