32a6f9105864ae05fa74d9aba17035d9fafbcd11
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-08-29 08:04:00 -0400

    Moved collections (displays previews of item categories) in shop.component.jsx to a new component called collections-overview.component.jsx - this is because shop.component.jsx will contain nested routes for the different categories, on this page users can click on a category on the /shop page and will be taken to that category

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import {selectCollections} from '../../redux/shop/shop.selectors';
+
+import './collections-overview.styles.scss';
+
+//Displays item categories and several images for that category
+const CollectionsOverview = ({collections}) => (
+  <div className='collections-overview'>
+    {collections.map(({id, ...otherCollectionProps}) =>(
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+  </div>
+)
+
+//Maps the memoized selector to this component to display items on screen (mapped under the collections prop)  - in a memozied selector the data is cached which helps to improve performance as the same calculations do not have to be performed multiple times and reduces the amount of re-renders as the cached result is the same
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import {selectCollections} from '../../redux/shop/shop.selectors';
-
-//Displays images and titles on the shop page
 const ShopPage = ({collections}) =>(
     <div className='shop-page'>
-      {collections.map(({id, ...otherCollectionProps}) =>(
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+      <CollectionsOverview />
     </div>
 );
 
-//Maps the memoized selector to this component to display items on screen (mapped under the collections prop)  - in a memozied selector the data is cached which helps to improve performance as the same calculations do not have to be performed multiple times and reduces the amount of re-renders as the cached result is the same
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
