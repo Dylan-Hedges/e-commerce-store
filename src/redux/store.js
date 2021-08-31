93b56737e9ab488c753bf0ae3e1ba500ecf1ea3e
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-08-31 07:19:25 -0400

    Removed Redux logger from production build - uses process.env.NODE_ENV to determine environment, if environment is in production then it pushes the logger to the middlewares array, if not then it doesnt add anything to the middlewares array

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,16 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 //Middleware that logs all action activity - used for debugging, catches and displays actions that get dispatched
-const middlewares = [logger];
+const middlewares = [];
 
-//Creates the Redux Store object - creates it using the object returned from rootReducer, passes it to <Provider> in index.js
+//If we are in development then push the logger into our array of middlewares - if it is anything else e.g. 'production' then it doesnt push it to the array as we dont want the logger in production
+if(process.env.NODE_ENV === 'development'){
+  console.log('test');
+  middlewares.push(logger);
+}
+
+
+//Creates the Redux Store object - executes middlewares (if in production contains logger that tracks Redux state changes, actions), creates it using the object returned from rootReducer, passes it to <Provider> in index.js
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 //Creates a persisted version of the Redux Store
